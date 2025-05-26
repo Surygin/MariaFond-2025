b5d5dbe66e79d3d799a0f954e1d197f51131a786
Anton Surygin <surygin@live.com>
2025-05-26 15:09:22 +0300

    Сделал необходимые фабрики и сиды для первоначальной работы приложения

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::firstOrCreate([
+            'email' => 'anton@premier-partner.ru'
+        ],
+            [
+                'name' => 'Super User',
+                'password' => Hash::make('1234')
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            MyCompanySeeder::class,
+            KidSeeder::class
         ]);
+
+
     }
 }
