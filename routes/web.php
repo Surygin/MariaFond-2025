9910bb78699c23b100ba7e01b243b0bfdcb42105
Anton Surygin <surygin@live.com>
2025-05-30 14:55:18 +0300

    Поправил роуты

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,18 @@ Route::get('/', function () {
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'auth'])->name('login.check');
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
-Route::get('profile/psw-edit', [ProfileController::class, 'editPassword'])->name('profile.edit.password');
-Route::put('profile', [ProfileController::class, 'updatePassword'])->name('profile.update.password');
+
 
 Route::prefix('admin')
     ->middleware([MyAuthMiddleware::class])
     ->group(function () {
+
+        #start profile routes
+        Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+        Route::get('profile', [ProfileController::class, 'show'])->name('profile.show');
+        Route::get('profile/psw-edit', [ProfileController::class, 'editPassword'])->name('profile.edit.password');
+        Route::put('profile', [ProfileController::class, 'updatePassword'])->name('profile.update.password');
+        #end profile routes
+
         Route::resource('kids', KidController::class);
 });
