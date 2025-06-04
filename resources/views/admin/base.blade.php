645f5774028e018b16ea23bd57357ea5b7209ad9
Anton Surygin <surygin@live.com>
2025-06-04 16:11:37 +0300

    Написал скрипт для меню

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -31,8 +31,8 @@
         <div class="row">
             <div class="col-lg-4 col-12">
                 @auth()
-                    <div class="sidebar">
-                        <p class="d-lg-none menu-button"><span></span></p>
+                    <div class="sidebar" id="sidebar">
+                        <p class="d-lg-none menu-button" id="menu-button"><span></span></p>
                         <ul class="menu">
                             <!-- HOME -->
                             <li><a href="{{ route('profile.show') }}">
@@ -60,14 +60,22 @@
                             <!-- END NEWS -->
 
                             <!-- REQUISITES -->
-                            <li><a href="#">
+                            <li><a href="/admin/about">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
+                                    </svg>
+                                    О комапании</a></li>
+                            <!-- END REQUISITES -->
+
+                            <!-- REQUISITES -->
+                            <li><a href="/admin/requisites">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
                                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
                                     </svg>
                                     Реквизиты</a></li>
                             <!-- END REQUISITES -->
 
-                            <li><a href="#">
+                            <li><a href="/admin/contacts">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
                                         <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                                     </svg>
@@ -103,7 +111,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <p>&copy;2025 сайт разработал <a href="#">Антон Сурыгин</a>.</p>
+                <p>&copy;2025 сайт разработал <a href="https://vk.com/suryginanton">Антон Сурыгин</a>.</p>
             </div>
             <!-- /.col-12 -->
         </div>
@@ -112,5 +120,20 @@
     <!-- /.container -->
 </footer>
 <!-- /.footer -->
+
+<script>
+    // Скрипт для меню
+    const menuBtn = document.getElementById('menu-button');
+    const sidebar = document.getElementById('sidebar');
+
+    menuBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        menuBtn.classList.toggle('menu-button__active');
+        sidebar.classList.toggle('w-show');
+    });
+    // Конец скрипта меню
+
+
+</script>
 </body>
 </html>
